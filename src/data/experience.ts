bb76d8af61aed4cde2d029af2234f5f0c6df99ce
Nicolas Hernandez <69226118+NicolasAHF@users.noreply.github.com>
2025-07-11 12:03:38 -0300

    Update experience.ts

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -1,5 +1,3 @@
-// src/data/experience.ts
-
 export interface ExperienceItem {
     role: string;
     company: string;
@@ -15,7 +13,7 @@ export interface ExperienceItem {
       company: "Oracle",
       logo: "/images/oracle.webp", 
       year: "Jul 2024 - Present",
-      description: "Leading a team of developers and building scalable web applications.",
+      description: "• Developed and implemented end-to-end functionalities, using an internal framework based on React (UIF) and TypeScript for the frontend, and Java for backend services.\n• Increased unit test coverage for the checkout module from 0% to 93%, by using an internal AI tool and prompt engineering.\n• Actively collaborated in cross-functional teams to define, design, and deliver new software requirements.",
       color: '#C74634'
     },
     {
@@ -23,7 +21,7 @@ export interface ExperienceItem {
       company: "UKG",
       logo: "/images/ukg.webp",
       year: " Feb 2024 - Jul 2024",
-      description: "Worked on microservices and cloud solutions.",
+      description: "• Executed automated and manual tests for web applications, ensuring product functionality and performance.\n• Closely collaborated with development teams for the identification, documentation, and resolution of software defects.",
       color: '#005151'
     },
     {
@@ -31,7 +29,7 @@ export interface ExperienceItem {
       company: "Oracle",
       logo: "/images/oracle.webp",
       year: " Apr 2022 - Feb 2024",
-      description: "Assisted in building MVP features for a SaaS platform.",
+      description: "• Created an end-to-end testing framework from scratch for the SuiteCommerce platform using Playwright and TypeScript.\n• Implemented automated testing that achieved 100% coverage of critical checkout flows.\n• Contributed to the improvement of CI/CD processes to streamline software deployment.",
       color: '#C74634'
     },
     {
@@ -39,7 +37,7 @@ export interface ExperienceItem {
         company: "Globant",
         logo: "/images/globant.webp",
         year: "Jun 2021 - Apr 2022",
-        description: "Assisted in building MVP features for a SaaS platform.",
+        description: "• Analyzed game mechanics and executed test plans to identify, report, and follow up on defects in collaboration with the development team.",
         color: '#BFD732'
     },
     {
@@ -47,7 +45,7 @@ export interface ExperienceItem {
         company: "TCS",
         logo: "/images/tcs.webp",
         year: "Set 2020 - Jun 2021",
-        description: "Assisted in building MVP features for a SaaS platform.",
+        description: "• Executed manual test cases and actively participated in Agile ceremonies to ensure software quality according to requirements.",
         color: '#FE0307'
     },
   ];
